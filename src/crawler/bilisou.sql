ab8a90bc94e66f5b9c2fcf76bc79788855315859
Gengsheng Liu <gsliu@freewheel.tv>
2017-02-10 02:47:01 +0800

    update schema

--- src/crawler/bilisou.sql
+++ src/crawler/bilisou.sql
@@ -1,56 +1,91 @@
-/*
-Navicat MySQL Data Transfer
+-- MySQL dump 10.13  Distrib 5.7.15, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: baidu
+-- ------------------------------------------------------
+-- Server version	5.7.15
 
-Source Server         : mysql
-Source Server Version : 50517
-Source Host           : localhost:3306
-Source Database       : baidu
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50517
-File Encoding         : 65001
+--
+-- Table structure for table `avaiuk`
+--
 
-Date: 2016-12-11 16:54:00
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for avaiuk
--- ----------------------------
 DROP TABLE IF EXISTS `avaiuk`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `avaiuk` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `uk` bigint(20) DEFAULT NULL,
   `flag` int(1) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for sharedata
--- ----------------------------
+--
+-- Table structure for table `sharedata`
+--
+
 DROP TABLE IF EXISTS `sharedata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sharedata` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
-  `shareid` varchar(64) DEFAULT NULL,
+  `share_id` varchar(100) DEFAULT NULL,
   `uinfo_id` bigint(20) DEFAULT NULL,
   `category` char(2) DEFAULT NULL,
-  `album_id` varchar(32) DEFAULT NULL,
+  `album_id` varchar(100) DEFAULT NULL,
+  `last_scan` bigint(11) DEFAULT NULL,
+  `size` int(100) DEFAULT NULL,
+  `view_count` int(11) DEFAULT NULL,
+  `like_count` int(11) DEFAULT NULL,
+  `file_count` int(11) DEFAULT NULL,
+  `filenames` varchar(1000) DEFAULT NULL,
+  `data_id` varchar(100) DEFAULT NULL,
+  `feed_time` bigint(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `uinfoid` (`uinfo_id`),
   CONSTRAINT `uinfoid` FOREIGN KEY (`uinfo_id`) REFERENCES `uinfo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8503 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Table structure for uinfo
--- ----------------------------
+--
+-- Table structure for table `uinfo`
+--
+
 DROP TABLE IF EXISTS `uinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `uinfo` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `uk` bigint(20) DEFAULT NULL,
   `uname` varchar(255) DEFAULT NULL,
   `avatar_url` varchar(255) DEFAULT NULL,
+  `fans_count` int(11) DEFAULT NULL,
+  `follow_count` int(11) DEFAULT NULL,
+  `intro` varchar(1000) DEFAULT NULL,
+  `pubshare_count` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `uk` (`uk`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-10  2:46:30
