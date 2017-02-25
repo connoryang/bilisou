60eff6a75fa52509728d37402f07ed9511714ded
Gengsheng Liu <gsliu@freewheel.tv>
2017-02-25 11:15:59 +0800

    update keyword

--- src/model/keyword.go
+++ src/model/keyword.go
@@ -8,14 +8,14 @@ import (
 )
 
 func KeywordHit(db *sql.DB, keyword string) {
-	count := 1
+	count := -1
 	rows, _ := db.Query("select count from keyword where keyword = ?", keyword)
 	for rows.Next() {
 		rows.Scan(&count)
 		count = count + 1;
 	}
 
-	if count == 1 {
+	if count == -1 {
 		stmt, _ := db.Prepare("update keyword set count = ?  where keyword = ?")
 		stmt.Exec(count, keyword)
 		stmt.Close()
